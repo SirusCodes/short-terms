f7a84b1db786f9c8fb13ff8414d70f5019165083
SirusCodes <darshandrander@gmail.com>
2022-04-19 22:05:38 +0530

    add cors mini project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 from summazier import get_summary
 
@@ -11,6 +12,14 @@ class SummaryRequest(BaseModel):
     text: str
     length: Optional[int] = 40
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/summarize")
 def summarize(request: SummaryRequest):
