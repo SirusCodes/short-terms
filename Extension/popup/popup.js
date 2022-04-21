c275a96d59a2352e02884aa7106da9632cb365af
Pranav Dani <pranavpdani@gmail.com>
2022-04-22 05:04:05 +0530

    feat: Summarize page with url feat: Summary Page
    Co-authored-by: Darshan Rander <darshandrander@gmail.com>

--- Extension/popup/popup.js
+++ Extension/popup/popup.js
@@ -1,41 +1,26 @@
-// document.addEventListener("DOMContentLoaded", async () => {
-//   const list = document.getElementById("list");
-//   const url = "https://v2.jokeapi.dev/joke/Any";
-
-//   try {
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     const joke = data.setup + " " + " " + data.delivery;
-//     const jokeElement = document.createElement("li");
-//     jokeElement.innerText = joke;
-//     list.appendChild(jokeElement);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// });
 document.getElementById("form").addEventListener("submit", (e) => {
-    e.preventDefault();
+  const content = document.getElementById("content");
 
-    const content = document.getElementById("content");
-    const length = document.getElementById("length").value;
+  chrome.tabs.create({ url: "summary/summary.html" });
+  chrome.runtime.sendMessage({
+    action: "setOptionsData",
+    data: {
+      type: "fromText",
+      text: content.value.replaceAll("\n", "\\n"),
+    },
+  });
+});
 
-    fetch("https://lqb1ye.deta.dev/summarize", {
-            method: "POST",
-            body: JSON.stringify({
-                text: content.value.replaceAll("\n", "\\n"),
-                length: length,
-            }),
-            // credentials: "include",
-            mode: "cors",
-            headers: {
-                "Content-type": "application/json",
-            },
-        })
-        .then((res) => res.json())
-        .then((res) => {
-            document.getElementById("response").innerText = res.summary;
-        })
-        .catch((res) => {
-            document.getElementById("response").innerText = JSON.stringify(res);
-        });
+document.getElementById("summarize-page").addEventListener("click", (e) => {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    const currentPage = tabs[0].url;
+    chrome.tabs.create({ url: "summary/summary.html" });
+    chrome.runtime.sendMessage({
+      action: "setOptionsData",
+      data: {
+        type: "fromURL",
+        url: currentPage,
+      },
+    });
+  });
 });
